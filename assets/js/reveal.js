59854507836a6308e4c57ab9982727855d8eae0f
Krynski <krynski08@gmail.com>
2020-02-17 22:04:50 +0100

    Firefox bug, + rajout d'un footer

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -4,11 +4,14 @@ const options = {
     threshold : .7
 }
 
+
+
 const handleIntersection = (entries, observer) => {
     entries.forEach((entry) => {
         if(entry.intersectionRatio > options.threshold){
         console.log("70% visible");
         entry.target.classList.add("reveal-visible")
+        robot.classList.add("translateBot")
         observer.unobserve(entry.target)
     }
         // else{    
@@ -19,7 +22,5 @@ const handleIntersection = (entries, observer) => {
     console.log('Handle Intersect');
     
 }
-
-const observer = new IntersectionObserver(handleIntersection, options)
 
 document.querySelectorAll(".reveal").forEach((reveal) => observer.observe(reveal))
